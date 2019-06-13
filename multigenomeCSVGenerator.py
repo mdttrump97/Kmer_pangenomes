fc1c9861729bab28b7e760dfb1e7d3f638c11614
Matthew Thompson <mdttrump97@bellsouth.net>
2019-06-13 16:33:15 -0500

    Adds method to quickly get subset of kmer counts

--- multigenomeCSVGenerator.py
+++ multigenomeCSVGenerator.py
@@ -11,7 +11,7 @@ def get_kmer_subset(amino_acid_subset, kmer_list):
             kmer_subset.append(kmer)
     return kmer_subset
 
-def sort_kmer_counts(kmer_counts, protein):
+def sort_kmer_counts(kmer_counts):
     kmer_count_tuple = list(zip(kmer_counts.index,kmer_counts))
     sorted_kmer_counts = sorted(kmer_count_tuple, key=lambda item: (-item[1], item[0]))
 
@@ -19,6 +19,24 @@ def sort_kmer_counts(kmer_counts, protein):
     sorted_kmer_counts.index = sorted_kmer_counts[0]
     del sorted_kmer_counts[0]
     return sorted_kmer_counts
+
+def append_kmer_subset(amino_acid_subset, kmer_counts, protein_vector, top_x):
+    selected_kmers = get_kmer_subset(amino_acid_subset, kmer_counts.index)
+    #print("Selected kmers: " + str(len(selected_kmers)))
+    if(len(selected_kmers) > top_x):
+        selected_kmer_counts = kmer_counts.loc[selected_kmers,]
+    
+        for kmer in selected_kmer_counts.index:
+            if kmer in protein_vector.index:
+                selected_kmer_counts = selected_kmer_counts.drop(kmer)
+        
+        sorted_selected_kmer_counts = sort_kmer_counts(selected_kmer_counts)[1]
+        
+        #print("selected kmers: ")
+        #print(str(sorted_selected_kmer_counts[0:3]))
+        # how many top occuring selected kmers?
+        protein_vector = protein_vector.append(sorted_selected_kmer_counts[0:top_x])
+    return protein_vector
 
 kmer_list = pd.read_csv("/Users/matthewthompson/Documents/UAMS_SURF/K-mer_testing/CSV_files/10_genome_4mer_counts_sparse/4mer_list.csv")
 del kmer_list["Unnamed: 0"]
@@ -52,30 +70,20 @@ for file in file_list:
         kmer_counts = nonzero_kmer_df[protein]
         
         kmer_count_tuple = list(zip(kmer_counts.index,kmer_counts))
-        sorted_kmer_counts = sort_kmer_counts(kmer_counts, protein)
+        sorted_kmer_counts = sort_kmer_counts(kmer_counts)[1]
         
         # how many top occuring kmers?
         protein_vector = sorted_kmer_counts[0:top_x]
         #print("top 3 vector:")
         #print(protein_vector)
         
-        selected_kmers = get_kmer_subset(["F", "Y", "W", "H"], kmer_counts.index)
-        #print("Selected kmers: " + str(len(selected_kmers)))
-        if(len(selected_kmers) > top_x):
-            selected_kmer_counts = kmer_counts.loc[selected_kmers,]
+        #protein_vector = append_kmer_subset(["F","Y","W","H"], kmer_counts, protein_vector, top_x)
+        #print("top 3 + aromatics:")
+        #print(protein_vector)
+        protein_vector = append_kmer_subset(["C","M"], kmer_counts, protein_vector, top_x)
         
-            for kmer in selected_kmer_counts.index:
-                if kmer in protein_vector.index:
-                    selected_kmer_counts = selected_kmer_counts.drop(kmer)
-            
-            sorted_selected_kmer_counts = sort_kmer_counts(selected_kmer_counts, protein)
-            
-            #print("selected kmers: ")
-            #print(str(sorted_selected_kmer_counts[0:3]))
-            # how many top occuring selected kmers?
-            protein_vector = protein_vector.append(sorted_selected_kmer_counts[0:3])
         dicty[protein] = protein_vector
-        #print("top 3 + top 3 selected vector:")
+        #print("top 3 + aromatics + CM")
         #print(protein_vector)
         #print(protein)
         #print("------------------------------------------------")
@@ -86,10 +94,10 @@ top_x_df = top_x_df.fillna(0)
 print("Converting to matrix market format")
 top_x_coo = sparse.coo_matrix(top_x_df)
 print("Writing to file")
-io.mmwrite('/Users/matthewthompson/Documents/UAMS_SURF/K-mer_testing/CSV_files/10_genome_4mer_counts_sparse/4mer_top_3_all_and_selected_table.mtx', top_x_coo)
-with open('/Users/matthewthompson/Documents/UAMS_SURF/K-mer_testing/CSV_files/10_genome_4mer_counts_sparse/top_3_all_and_selected_kmers.csv', 'w') as writeFile:
+io.mmwrite('/Users/matthewthompson/Documents/UAMS_SURF/K-mer_testing/CSV_files/10_genome_4mer_counts_sparse/4mer_top_3_all_CM_table.mtx', top_x_coo)
+with open('/Users/matthewthompson/Documents/UAMS_SURF/K-mer_testing/CSV_files/10_genome_4mer_counts_sparse/top_3_all_CM_kmers.csv', 'w') as writeFile:
     writer = csv.writer(writeFile)
     writer.writerow(top_x_df.index)
-with open('/Users/matthewthompson/Documents/UAMS_SURF/K-mer_testing/CSV_files/10_genome_4mer_counts_sparse/top_3_all_and_selected_protein_list.csv', 'w') as writeFile:
+with open('/Users/matthewthompson/Documents/UAMS_SURF/K-mer_testing/CSV_files/10_genome_4mer_counts_sparse/top_3_all_CM_protein_list.csv', 'w') as writeFile:
     writer = csv.writer(writeFile)
     writer.writerow(top_x_df.columns)
