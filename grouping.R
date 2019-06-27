bc39001d85c65570e984a1d1b0fbdb59f8cabe21
Matthew Thompson <mdttrump97@bellsouth.net>
2019-06-27 10:09:35 -0500

    Adds pan-genome evo plot to git diff

--- grouping.R
+++ grouping.R
@@ -2,10 +2,13 @@ library("FindMyFriends")
 library("kebabs")
 library("Matrix")
 
-setwd("/Users/matthewthompson/Documents/UAMS_SURF/K-mer_testing/FAA_files/")
+setwd("/Users/matthewthompson/Documents/UAMS_SURF/K-mer_testing/FAA_files/20_genomes")
 genomeFiles <- list.files(getwd(), full.names=TRUE, pattern='*.faa')
-pan <- pangenome(genomeFiles[1:10], translated=TRUE, geneLocation='prodigal', lowMem=FALSE)
-setwd("/Users/matthewthompson/Documents/UAMS_SURF/K-mer_testing/CSV_files/")
-groupingList <- as.integer(read.csv("10_genome_top_5_4mer_full_alphabet_cosine_grouping_list_0.5_ps.csv", header=FALSE, sep=',', stringsAsFactors = FALSE))
+pan <- pangenome(genomeFiles[1:20], translated=TRUE, geneLocation='prodigal', lowMem=FALSE)
+setwd("/Users/matthewthompson/Documents/UAMS_SURF/K-mer_testing/CSV_files/20_genomes/")
+groupingList <- as.integer(read.csv("20_genome_4mer_top_5_all_FYWH_CM_grouping_list.csv", header=FALSE, sep=',', stringsAsFactors = FALSE))
 pan <- manualGrouping(pan, groupingList)
+defaults(pan)$coreThreshold <- 0.9
 plotStat(pan, type='qual', palette = 6)
+plotEvolution(pan)
+pan
