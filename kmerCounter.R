34418d40c1c8fceb276090d7b7fb343d5784cc5b
Matthew Thompson <mdttrump97@bellsouth.net>
2019-07-22 09:03:14 -0500

    Documents kmerCounter.R

--- kmerCounter.R
+++ kmerCounter.R
@@ -3,16 +3,20 @@ library("kmer")
 library("Matrix")
 library("FindMyFriends")
 
-input_folder = "/Users/matthewthompson/Documents/UAMS_SURF/K-mer_testing/FAA_files/14_genomes/"
-folder_name = "FAA_files/14_genomes/"
-output_folder = "/Users/matthewthompson/Documents/UAMS_SURF/K-mer_testing/CSV_files/14_genomes/"
+# Format input and output
+input_folder = "/Users/matthewthompson/Documents/UAMS_SURF/K-mer_testing/FAA_files/phylotypeA/"
+folder_name = "FAA_files/phylotypeA/"
+output_folder = "/Users/matthewthompson/Documents/UAMS_SURF/K-mer_testing/CSV_files/phylotypeA/"
+
+# Set k-mer length
 kmer_length = 3
 kmer_string = paste(toString(kmer_length), "mer", sep = '')
-  
+
+# Read in .faa files after running Prodigal on genomic FASTA (.fna) files
 setwd(input_folder)
 genome_files <- list.files(getwd(), full.names=TRUE, pattern='*.faa')
 
-# Output gene ordering from FindMyFriends for canopyClustering.py output
+# Output ordering of genes from FindMyFriends to combine with canopyClustering.py output
 pan <- pangenome(genome_files[1:length(genome_files)], translated=TRUE, geneLocation='prodigal', lowMem=FALSE)
 grouping_list <- c()
 for(x in pan@sequences@ranges@NAMES)
@@ -25,19 +29,25 @@ for (file in genome_files)
 {
   proteins <- read.FASTA(file, type = "AA")
   print(paste('Genome ',file,' proteins are loaded'))
+  
+  # Count k-mers in each protein sequence
   kmerCounts <- kcount(proteins, k = kmer_length, compress = FALSE)
   print(paste('Genome ',file, ' kmerCounts is finished'))
+  # Convert to a sparse storage format to reduce file size when storing
   sparseKmerCounts <- Matrix(kmerCounts, sparse = TRUE)
   
+  # Format output file name
   file_name <- strsplit(file, ".faa")
   file_name <- strsplit(file_name[[1]][1], folder_name)
   file_name <- paste(output_folder, file_name[[1]][2], sep = '')
   out_path <- paste(file_name,paste('_', kmer_string, sep = ''),sep = '')
-  out_path <- paste(out_path, '_count_matrix_full_alphabet.csv',sep = '')
+  out_path <- paste(out_path, '_count_matrix_full_alphabet.mtx',sep = '')
   
   protein_out_path <- paste(strsplit(file_name, paste(kmer_string, "_count", sep = ''))[[1]][1], "_protein_list.csv", sep = '')
 
   setwd(output_folder)
+  # Output to be used in kmerSelector.py
+  # Write matrix out in matrix market format
   writeMM(sparseKmerCounts, file=out_path)
   write.csv(rownames(kmerCounts), file = protein_out_path)
   write.csv(colnames(kmerCounts), file = paste(kmer_string, "_list.csv", sep = ''))
