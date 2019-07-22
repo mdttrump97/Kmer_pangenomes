85df95159da9b75f4fc2c052326b50ec77715d17
Matthew Thompson <mdttrump97@bellsouth.net>
2019-07-22 09:48:15 -0500

    Documents grouping.R

--- grouping.R
+++ grouping.R
@@ -1,14 +1,22 @@
 library("FindMyFriends")
-library("kebabs")
-library("Matrix")
 
-setwd("/Users/matthewthompson/Documents/UAMS_SURF/K-mer_testing/FAA_files/20_genomes")
+# Read in .faa files (same input as kmerCounter.R)
+setwd("/Users/matthewthompson/Documents/UAMS_SURF/K-mer_testing/FAA_files/medioid_dataset")
 genomeFiles <- list.files(getwd(), full.names=TRUE, pattern='*.faa')
-pan <- pangenome(genomeFiles[1:20], translated=TRUE, geneLocation='prodigal', lowMem=FALSE)
-setwd("/Users/matthewthompson/Documents/UAMS_SURF/K-mer_testing/CSV_files/20_genomes/")
-groupingList <- as.integer(read.csv("20_genome_4mer_top_5_all_FYWH_CM_grouping_list.csv", header=FALSE, sep=',', stringsAsFactors = FALSE))
+pan <- pangenome(genomeFiles[1:length(genomeFiles)], translated=TRUE, geneLocation='prodigal', lowMem=FALSE)
+
+# Set core gene group threshold
+#defaults(pan)$coreThreshold <- 0.9
+
+setwd("/Users/matthewthompson/Documents/UAMS_SURF/K-mer_testing/CSV_files/medioid_3mers/")
+
+# Read in cluster membership from canopyClustering.py
+groups <- read.csv('medioid_3mer_top_9_0.675_grouping_list.csv', sep = ',')
+groupingList <- groups[,"clust"]
 pan <- manualGrouping(pan, groupingList)
-defaults(pan)$coreThreshold <- 0.9
+
+# Plot pie chart of pan-genome composition
 plotStat(pan, type='qual', palette = 6)
+# Plot pan-genome composition as genomes are added
 plotEvolution(pan)
 pan
